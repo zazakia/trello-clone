499cca908489534c5c5be4ea4f12a70dc390a2ae
zazakia <cybergada@gmail.com>
2025-08-03 05:38:42 +0800

    Fix unused import in BoardSelector

--- src/components/BoardSelector.tsx
+++ src/components/BoardSelector.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useMemo } from 'react';
 import { useBoard } from '../hooks/useBoard';
 import { ConnectionTest } from './ConnectionTest';
 import { Search, Plus, X } from 'lucide-react';
-import { useBoardNotifications } from '../hooks/useBoardNotifications';
 
 interface BoardSelectorProps {
   onSelectBoard: (boardId: string) => void;
